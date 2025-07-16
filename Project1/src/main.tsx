466bb781a7849c136996c9e12ece8226916607d5
eraera <126274607+Ataulla56@users.noreply.github.com>
2025-07-16 18:10:49 +0600

    Update main.tsx

--- Project1/src/main.tsx
+++ Project1/src/main.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { AirdropClaim } from './AirdropClaim';
+import AirdropClaim from './AirdropClaim';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AirdropClaim
-      reward={{ label: "Airdrop", icon: "🎁" }}
-      walletAddress="YOUR_WALLET_ADDRESS"
-      onClaimComplete={() => alert('Claim complete!')}
-    />
+    <AirdropClaim />
   </React.StrictMode>
 );
